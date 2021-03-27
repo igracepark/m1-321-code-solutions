c17295f052d58e6758764eb38b0aa8aa84ad6fd2
Grace Park <haeeunee@gmail.com>
2021-03-27 12:27:08 -0700

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 2;
+var height = 4;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 80;
+var payment = 100;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 75;
+var midterm = 80;
+var final = 90;
+var grade = ((quizzes + midterm + final) / 3);
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Grace';
+var lastName = 'Park';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+var pH = 4;
+var isAcidic = (pH < 7);
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 30;
+var isSparta = (headCount === 300);
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
