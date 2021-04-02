b1448b549335d905f37ea3888101d3462e4ce55e
Grace Park <haeeunee@gmail.com>
2021-04-01 21:22:33 -0700

    Completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,74 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderDate: 'August 4, 2020',
+    delivered: true,
+    deliverDate: 'Aug 8, 2020',
+    totalCartCost: 34.00,
+    shipToName: 'JS Masher',
+    purchasedItems: [
+      {
+        productName: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnClosedDate: 'Sep 7, 2020',
+        itemCost: 31.55
+      }
+    ]
+  },
+
+  {
+    orderNumber: '113-9984268-1280257',
+    orderDate: 'July 19, 2020',
+    delivered: true,
+    deliverDate: 'Jul 20, 2020',
+    totalCartCost: 44.53,
+    shipToName: 'JS Masher',
+    purchasedItems: [
+      {
+        productName: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnClosedDate: 'Aug 19, 2020',
+        itemCost: 41.33
+      }
+    ]
+  },
+
+  {
+    orderNumber: '114-2875557-9059409',
+    orderDate: 'July 4, 2020',
+    delivered: true,
+    deliverDate: 'Jul 7, 2020',
+    totalCartCost: 17.22,
+    shipToName: 'JS Masher',
+    purchasedItems: [
+      {
+        productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        returnClosedDate: 'Aug 5, 2020',
+        itemCost: 15.98
+      }
+    ]
+  },
+
+  {
+    orderNumber: '113-2883177-2648248',
+    orderDate: 'July 3, 2020',
+    delivered: true,
+    deliverDate: 'Jul 5, 2020',
+    totalCartCost: 138.93,
+    shipToName: 'JS Masher',
+    purchasedItems: [
+      {
+        productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnClosedDate: 'Aug 4, 2020',
+        itemCost: 94.95
+      },
+
+      {
+        productName: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnClosedDate: 'Aug 4, 2020',
+        itemCost: 33.99
+      }
+    ]
+  }
+];
