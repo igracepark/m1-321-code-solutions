9f9fca90649b9ec3ff34a1cb55c2f16d411f73f0
Grace Park <haeeunee@gmail.com>
2021-04-15 19:39:09 -0700

    Completed dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,14 @@
+console.log('hello, world');
+var heading = document.querySelector('h1');
+console.log('h1:', heading);
+console.dir(heading);
+var explanation = document.querySelector('#explanation');
+console.log('explanation', explanation);
+console.dir(explanation);
+var hint = document.querySelector('.hint');
+console.log('hint', hint);
+console.dir(hint);
+var paragraph = document.querySelectorAll('p');
+console.log('paragraph', paragraph);
+var exampleLink = document.querySelectorAll('.example-link');
+console.log('exampleLink', exampleLink);
