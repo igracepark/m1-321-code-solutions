37f2c347672e48f2d1ef410294e8865c25f1f13b
Grace Park <haeeunee@gmail.com>
2021-04-17 12:14:53 -0700

    e

---
+++ dom-manipulation/main.js
@@ -0,0 +1,30 @@
+var count = 1;
+var $hotButton = document.querySelector('.hot-button');
+var $clickCount = document.querySelector('.click-count');
+
+function handleClick(event) {
+  $clickCount.textContent = 'Clicks: ' + count++;
+  if (count > 4 && count < 7) {
+    $hotButton.className = 'hot-button ' + 'cool';
+  } else {
+    if (count > 7 && count < 10) {
+      $hotButton.className = 'hot-button ' + 'tepid';
+    } else {
+      if (count > 10 && count < 13) {
+        $hotButton.className = 'hot-button ' + 'warm';
+      } else {
+        if (count > 13 && count < 16) {
+          $hotButton.className = 'hot-button ' + 'hot';
+        } else {
+          if (count > 16) {
+            $hotButton.className = 'hot-button ' + 'nuclear';
+          }
+        }
+      }
+
+    }
+  }
+
+}
+
+$hotButton.addEventListener('click', handleClick);
